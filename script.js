ef17800d776984892ef27fa36344348e389a0442
Troy Dorman <troydorman@gmail.com>
2021-05-02 10:30:36 -0600

    added play and pause functionality

--- script.js
+++ script.js
@@ -26,6 +26,37 @@ function loadSong(song) {
     cover.src =`images/${song}.jpg`
 }
 
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play();
+
+}
+
+
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause();
+}
+
+
+// event listeners
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+    
+    if(isPlaying) {
+        pauseSong();
+
+    } else {
+        playSong();
+    }
+})
 
 
 
